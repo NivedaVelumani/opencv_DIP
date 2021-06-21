b3e945b73b6fda78be3317b25667912f65661878
NIVEDA V <73704016+NivedaVelumani@users.noreply.github.com>
2021-06-21 10:25:49 -0700

    Create text_over.py

---
+++ text_over.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy
+import imutils
+
+#img read and display
+img=cv2.imread("baby.jpg")
+
+
+
+
+# draw green text on the image
+output = img.copy()
+cv2.putText(output, "BABY", (180, 250),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 250, 0), 2)
+cv2.imshow("Text", output)
+cv2.waitKey(0)
